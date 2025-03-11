c614a2746e8c62fdd15970103be40e2d5a65481a
devrantukan <devrantukan@gmail.com>
2025-03-12 01:45:11 +0300

    chore: next config changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  basePath: '/emlak',
+  assetPrefix: '/emlak/',
   images: {
   loader: 'custom',
   loaderFile: './src/lib/supabaseImageLoader.ts',
