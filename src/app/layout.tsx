706c3267466a261a7ab4594fbcd86549004dcdfd
devrantukan <devrantukan@gmail.com>
2025-03-17 15:17:18 +0300

    fix: manifest url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,7 +93,7 @@ export default function RootLayout({
           sizes="16x16"
           href="/emlak/favicon/favicon-16x16.png"
         />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="manifest" href="/emlak/favicon/site.webmanifest" />
         <link
           rel="mask-icon"
           href="/emlak/favicon/safari-pinned-tab.svg"
